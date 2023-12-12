987ea073918bda7fb84e7b626fb274438dae92a7
emly666 <153506497+emly666@users.noreply.github.com>
2023-12-12 10:53:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+printf("1.: (a||b)")
+#2
+printf("2.: (a&&c)||(b&&!c)")
+#3
+printf("3.: 1")
+#4
+printf("4.: a||(!b&&!c)")
+#5
+printf("5.: (a&&!b)||(!b&&c)")
+#6
+printf("6.: a||(!c&&!d)")
+#7
+printf("7.: a&&b&&c")
+#8
+printf("8.: a&&b&&!c")
